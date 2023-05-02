263408ba843c2138e36899ea66fac58cf471f3be
Ahmed <ahmedmayara789@gmail.com>
2023-05-02 21:48:19 +0100

    feat: Adding the pagination component to the CategoriesView.vue page

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -8,6 +8,7 @@ import { useRouter } from 'vue-router';
 import moment from 'moment';
 import Modal from '../components/Modal.vue';
 import Toast from '../components/Toast.vue';
+import Pagination from '../components/Pagination.vue';
 import useCategories from '../composables/Categories.js';
 import MdiCheck from '~icons/mdi/check'
 import MdiClose from '~icons/mdi/close'
@@ -423,30 +424,7 @@ const showDeleteToaster = ref(false);
                 </tr>
             </table>
 
-            <nav v-if="meta.last_page > 1">
-                <ul class="flex justify-center mt-6">
-                    <li class="mx-3">
-                        <a href="#"
-                            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                            @click.prevent="goToPage(meta.current_page - 1)" :class="{'opacity-50 cursor-default': meta.current_page === 1}">Previous</a>
-                    </li>
-                    
-                    <template v-for="pageNumber in meta.last_page">
-                        <li class="mx-1">
-                            <a href="#"
-                                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                :class="{ 'opacity-50 cursor-not-allowed': meta.current_page === pageNumber }"
-                                @click.prevent="goToPage(pageNumber)">{{ pageNumber }}</a>
-                        </li>
-                    </template>
-                    
-                    <li class="mx-3">
-                        <a href="#"
-                            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                            @click.prevent="goToPage(meta.current_page + 1)" :class="{'opacity-50 cursor-default': meta.current_page === meta.last_page}">Next</a>
-                    </li>
-                </ul>
-            </nav>
+            <Pagination :meta="meta" :go-to-page="goToPage" class="mt-5" />
         </main>
 
         <Modal v-model:show="showDeleteConfirmationModal" title="Delete Category" @close="showDeleteConfirmationModal = false">
